f1a78396ef158d6e3b3282953f1c2aa08a934349
Mustafa Saeed <mustafasa3eed@yahoo.com>
2021-11-14 15:59:43 +0200

    slidable decoration

--- lib/ui/home/todo_widget.dart
+++ lib/ui/home/todo_widget.dart
@@ -6,53 +6,82 @@ class TodoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Slidable(
-      startActionPane: ActionPane(motion: ScrollMotion(), children: [
-        SlidableAction(
-        onPressed: null,
-        backgroundColor: Color(0xFFFE4A49),
-        foregroundColor: Colors.white,
-        icon: Icons.delete,
-        label: 'Delete',
-        )],),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: Colors.white,
-
-        ),
-        margin: EdgeInsets.symmetric(vertical: 10),
-        width: 352,
-        height: 115,
-        child: Row(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10),
-              width: 4,
-              height: 62,
-              color: Theme.of(context).primaryColor,
-            ),
-            Expanded(child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Play basketball',style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold
-                ),)
-              ],
-            )),
-            Container(
-              width: 69,
-              height: 34,
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                borderRadius: BorderRadius.circular(8)
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Slidable(
+        actionPane: SlidableDrawerActionPane(),
+        actions: [
+          IconSlideAction(
+            onTap: () {},
+            color: Colors.transparent,
+            iconWidget: Container(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 236, 75, 75),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      bottomLeft: Radius.circular(12))),
+              margin: EdgeInsets.symmetric(vertical: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    'Delete',
+                    style: TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
+                  )
+                ],
               ),
-              margin: EdgeInsets.all(10),
-              child: Icon(Icons.check,
-            color: Colors.white,),)
-          ],
+            ),
+          )
+        ],
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.white,
+          ),
+          margin: EdgeInsets.symmetric(vertical: 10),
+          width: 352,
+          height: 115,
+          child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                width: 4,
+                height: 62,
+                color: Theme.of(context).primaryColor,
+              ),
+              Expanded(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Play basketball',
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              )),
+              Container(
+                width: 69,
+                height: 34,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(8)),
+                margin: EdgeInsets.all(10),
+                child: ImageIcon(
+                  AssetImage('assets/images/icon-check.png'),
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
