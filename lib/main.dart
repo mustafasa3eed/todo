aad22afbb5a77e5dbaff4b9f787018861d7428bf
Mustafa Saeed <mustafasa3eed@yahoo.com>
2021-11-15 17:02:59 +0200

    android firebase installed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/ui/home/home_screen.dart';
 import 'package:todo/ui/home/theme.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
 
   // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
