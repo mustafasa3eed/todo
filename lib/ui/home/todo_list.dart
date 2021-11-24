1bb57d1fada4276d905bb7daae615815306d3dbd
bugs <mustafasa3eed98@gmail.com>
2021-11-24 08:38:57 +0200

    sheet decoration

--- lib/ui/home/todo_list.dart
+++ lib/ui/home/todo_list.dart
@@ -21,75 +21,77 @@ class _TodoListState extends State<TodoList> {
   @override
   Widget build(BuildContext context) {
     var provider = Provider.of<AppConfigProvider>(context);
-    return Container(
-      child: Column(
-        children: [
-          Container(
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(bottomRight: Radius.circular(10), bottomLeft: Radius.circular(10)),
             color: provider.isDarkMode()?MyThemeData.primaryColorDark:Colors.white,
-            child: TableCalendar(
-              selectedDayPredicate: (day) {
-               return isSameDay(day,selectedDay);
-              },
-              onDaySelected: (sDay, fDay) {
-                setState(() {
-                  selectedDay = sDay;
-                  focusedDay = fDay;
-                });
-              },
-              focusedDay: focusedDay,
-              firstDay: DateTime.now(),
-              lastDay: DateTime.now().add(Duration(days: 365)),
-              calendarFormat: CalendarFormat.week,
-              weekendDays: [],
-              calendarStyle: CalendarStyle(
-                  selectedTextStyle: TextStyle(
-                    color: Colors.white,
-                  ),
-                  selectedDecoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  todayTextStyle: TextStyle(
-                    color: Colors.black,
-                  ),
-                  todayDecoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.blue,
-                      width: 1.5,
-                    ),
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  defaultDecoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  defaultTextStyle: TextStyle(
-                    color: Colors.black,
-                  )),
-            ),
+
           ),
-          Expanded(
-            child: Scrollbar(
-              child: StreamBuilder<QuerySnapshot<task>>(
-                stream: getTasks().snapshots(),
-                builder: (BuildContext buildContext,AsyncSnapshot<QuerySnapshot<task>> snapshot ){
-                  if(snapshot.hasError){
-                   return Text(snapshot.error.toString());
-                  }else if(snapshot.connectionState == ConnectionState.waiting){
-                    return Center(child: CircularProgressIndicator());
-                  }
-                  List<task> item = snapshot.data!.docs.map((e) => e.data()).toList();
-                  return ListView.builder(itemBuilder: (buildContext, index){
-                    return TodoWidget(item[index]);
-                  },
-                  itemCount: item.length,);
-              }
-              ),
-            )
+          child: TableCalendar(
+            selectedDayPredicate: (day) {
+             return isSameDay(day,selectedDay);
+            },
+            onDaySelected: (sDay, fDay) {
+              setState(() {
+                selectedDay = sDay;
+                focusedDay = fDay;
+              });
+            },
+            focusedDay: focusedDay,
+            firstDay: DateTime.now(),
+            lastDay: DateTime.now().add(Duration(days: 365)),
+            calendarFormat: CalendarFormat.week,
+            weekendDays: [],
+            calendarStyle: CalendarStyle(
+                selectedTextStyle: TextStyle(
+                  color: Colors.white,
+                ),
+                selectedDecoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                todayTextStyle: TextStyle(
+                  color: Colors.black,
+                ),
+                todayDecoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue,
+                    width: 1.5,
+                  ),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                defaultDecoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                defaultTextStyle: TextStyle(
+                  color: Colors.black,
+                )),
+          ),
+        ),
+        Expanded(
+          child: Scrollbar(
+            child: StreamBuilder<QuerySnapshot<task>>(
+              stream: getTasks().snapshots(),
+              builder: (BuildContext buildContext,AsyncSnapshot<QuerySnapshot<task>> snapshot ){
+                if(snapshot.hasError){
+                 return Text(snapshot.error.toString());
+                }else if(snapshot.connectionState == ConnectionState.waiting){
+                  return Center(child: CircularProgressIndicator());
+                }
+                List<task> item = snapshot.data!.docs.map((e) => e.data()).toList();
+                return ListView.builder(itemBuilder: (buildContext, index){
+                  return TodoWidget(item[index]);
+                },
+                itemCount: item.length,);
+            }
             ),
-        ],
-      ),
+          )
+          ),
+      ],
     );
   }
 }
