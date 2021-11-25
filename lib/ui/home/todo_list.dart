9139dd05ae11f2519f1320657e09835bc4c3ba8f
bugs <mustafasa3eed98@gmail.com>
2021-11-25 05:57:28 +0200

    sheet decoration

--- lib/ui/home/todo_list.dart
+++ lib/ui/home/todo_list.dart
@@ -1,13 +1,11 @@
+import 'package:calendar_timeline/calendar_timeline.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
 import 'package:todo/data/firebase.dart';
 import 'package:todo/data/task.dart';
 import 'package:todo/providers/AppConfigProvider.dart';
-import 'package:todo/ui/home/theme.dart';
 import 'package:todo/ui/home/todo_widget.dart';
 import 'package:provider/provider.dart';
-
 
 class TodoList extends StatefulWidget {
   @override
@@ -24,73 +22,44 @@ class _TodoListState extends State<TodoList> {
     return Column(
       children: [
         Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(bottomRight: Radius.circular(10), bottomLeft: Radius.circular(10)),
-            color: provider.isDarkMode()?MyThemeData.primaryColorDark:Colors.white,
-
-          ),
-          child: TableCalendar(
-            selectedDayPredicate: (day) {
-             return isSameDay(day,selectedDay);
-            },
-            onDaySelected: (sDay, fDay) {
-              setState(() {
-                selectedDay = sDay;
-                focusedDay = fDay;
-              });
-            },
-            focusedDay: focusedDay,
-            firstDay: DateTime.now(),
-            lastDay: DateTime.now().add(Duration(days: 365)),
-            calendarFormat: CalendarFormat.week,
-            weekendDays: [],
-            calendarStyle: CalendarStyle(
-                selectedTextStyle: TextStyle(
-                  color: Colors.white,
-                ),
-                selectedDecoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                todayTextStyle: TextStyle(
-                  color: Colors.black,
-                ),
-                todayDecoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.blue,
-                    width: 1.5,
-                  ),
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                defaultDecoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                defaultTextStyle: TextStyle(
-                  color: Colors.black,
-                )),
-          ),
+          color: Colors.white,
+          child: CalendarTimeline(
+            initialDate: DateTime.now(),
+            firstDate: DateTime.now(),
+            lastDate: DateTime.now().add(Duration(days: 365)),
+            onDateSelected: (date) => print(date),
+            leftMargin: 20,
+            monthColor: Colors.blueGrey,
+            dayColor: Colors.teal[200],
+            activeDayColor: Colors.white,
+            activeBackgroundDayColor: Colors.redAccent[100],
+            dotsColor: Color(0xFF333A47),
+            selectableDayPredicate: (date) => date.day != 23,
+            locale: 'en_ISO',
+          )
         ),
         Expanded(
-          child: Scrollbar(
-            child: StreamBuilder<QuerySnapshot<task>>(
+            child: Scrollbar(
+          child: StreamBuilder<QuerySnapshot<task>>(
               stream: getTasks().snapshots(),
-              builder: (BuildContext buildContext,AsyncSnapshot<QuerySnapshot<task>> snapshot ){
-                if(snapshot.hasError){
-                 return Text(snapshot.error.toString());
-                }else if(snapshot.connectionState == ConnectionState.waiting){
+              builder: (BuildContext buildContext,
+                  AsyncSnapshot<QuerySnapshot<task>> snapshot) {
+                if (snapshot.hasError) {
+                  return Text(snapshot.error.toString());
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
                   return Center(child: CircularProgressIndicator());
                 }
-                List<task> item = snapshot.data!.docs.map((e) => e.data()).toList();
-                return ListView.builder(itemBuilder: (buildContext, index){
-                  return TodoWidget(item[index]);
-                },
-                itemCount: item.length,);
-            }
-            ),
-          )
-          ),
+                List<task> item =
+                    snapshot.data!.docs.map((e) => e.data()).toList();
+                return ListView.builder(
+                  itemBuilder: (buildContext, index) {
+                    return TodoWidget(item[index]);
+                  },
+                  itemCount: item.length,
+                );
+              }),
+        )),
       ],
     );
   }
