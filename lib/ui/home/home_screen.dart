2767591a2fd1934b00aaf2c08e5de6f0e4140a14
bugs <mustafasa3eed98@gmail.com>
2021-12-19 06:22:29 +0200

    new decoration

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -14,58 +14,55 @@ class HomeScreen extends StatefulWidget {
 }
 class _HomeScreenState extends State<HomeScreen> {
 
-  @override
   int currentIndex = 0;
+  @override
   Widget build(BuildContext context) {
     var provider = Provider.of<AppConfigProvider>(context);
-    return Directionality(
-      textDirection: TextDirection.ltr,
-      child: Scaffold(
-        backgroundColor: provider.isDarkMode()?MyThemeData.darkScaffoldBackground:MyThemeData.lightScaffoldBackground ,
-        appBar: AppBar(
-          elevation: 0,
-          title: const Text('Todo App'),
-          centerTitle: false,
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-          shape:  StadiumBorder(
-              side: BorderSide(
-                  color: provider.isDarkMode()?MyThemeData.primaryColorDark:Colors.white, width:4)),
-          child: const Icon(Icons.add),
-          onPressed: () {
-            newTaskSheet();
-          },
-        ),
-        bottomNavigationBar:
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8)
-              ),
-              child: BottomAppBar(
-                color: provider.isDarkMode()?MyThemeData.primaryColorDark:Colors.white,
-                shape: const CircularNotchedRectangle(),
-                notchMargin: 8,
-                clipBehavior: Clip.antiAlias,
-                child: BottomNavigationBar(
-                  backgroundColor: provider.isDarkMode()?MyThemeData.primaryColorDark:Colors.white,
-                  elevation: 0,
-                  currentIndex: currentIndex,
-                  onTap: (index){
-                    currentIndex= index;
-                    setState(() {});
-                  },
-                  showSelectedLabels: false,
-                  showUnselectedLabels: false,
-                  items: [
-                    const BottomNavigationBarItem(icon: Icon(Icons.list,size: 33,), label: ''),
-                    const BottomNavigationBarItem(icon: Icon(Icons.settings_outlined, size: 33,),label: ''),
-                  ],
-                ),
+    return Scaffold(
+      backgroundColor: provider.isDarkMode()?MyThemeData.darkScaffoldBackground:MyThemeData.lightScaffoldBackground ,
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Todo App'),
+        centerTitle: false,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        shape:  StadiumBorder(
+            side: BorderSide(
+                color: provider.isDarkMode()?MyThemeData.primaryColorDark:Colors.white, width:4)),
+        child: const Icon(Icons.add),
+        onPressed: () {
+          newTaskSheet();
+        },
+      ),
+      bottomNavigationBar:
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8)
+            ),
+            child: BottomAppBar(
+              color: provider.isDarkMode()?MyThemeData.primaryColorDark:Colors.white,
+              shape: const CircularNotchedRectangle(),
+              notchMargin: 8,
+              clipBehavior: Clip.antiAlias,
+              child: BottomNavigationBar(
+                backgroundColor: provider.isDarkMode()?MyThemeData.primaryColorDark:Colors.white,
+                elevation: 0,
+                currentIndex: currentIndex,
+                onTap: (index){
+                  currentIndex= index;
+                  setState(() {});
+                },
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                items: [
+                  const BottomNavigationBarItem(icon: Icon(Icons.list,size: 33,), label: ''),
+                  const BottomNavigationBarItem(icon: Icon(Icons.settings_outlined, size: 33,),label: ''),
+                ],
               ),
             ),
-        body: tabs[currentIndex],
-      ),
+          ),
+      body: tabs[currentIndex],
     );
   }
   List<Widget> tabs = [
--- lib/ui/home/todo_list.dart
+++ lib/ui/home/todo_list.dart
@@ -1,10 +1,10 @@
 import 'package:calendar_timeline/calendar_timeline.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/data/firebase.dart';
 import 'package:todo/data/task.dart';
 import 'package:todo/providers/AppConfigProvider.dart';
-import 'package:todo/ui/home/theme.dart';
 import 'package:todo/ui/home/todo_widget.dart';
 import 'package:provider/provider.dart';
 
@@ -16,58 +16,82 @@ class TodoList extends StatefulWidget {
 class _TodoListState extends State<TodoList> {
   DateTime selectedDay = DateTime.now();
   DateTime initialDay = DateTime.now();
+  @override
+  void initState() {
+    super.initState();
+    _resetSelectedDate();
+  }
+
+  void _resetSelectedDate() {
+    selectedDay = DateTime.now();
+  }
 
   @override
   Widget build(BuildContext context) {
     var provider = Provider.of<AppConfigProvider>(context);
     return Column(
       children: [
-        Container(
-            color: Colors.blue,
-            child: CalendarTimeline(
-              initialDate: selectedDay,
-              firstDate: DateTime.now(),
-              lastDate: DateTime.now().add(Duration(days: 365)),
-              onDateSelected: (day){
-                setState(() {
-                  selectedDay=day!;
-
-                });
-              },
-              leftMargin: 20,
-              monthColor: Colors.black,
-              dayColor: Colors.teal[200],
-              activeDayColor: Colors.white,
-              activeBackgroundDayColor: Colors.redAccent[100],
-              dotsColor: Color(0xFF333A47),
-              selectableDayPredicate: (date) => date.day != 23,
-              locale: 'en_ISO',
-            )
+        CalendarTimeline(
+          showYears: true,
+          initialDate: selectedDay,
+          firstDate: DateTime.now(),
+          lastDate: DateTime.now().add(Duration(days: 365)),
+          onDateSelected: (date) {
+            setState(() {
+              selectedDay = date!;
+            });
+          },
+          leftMargin: 20,
+          monthColor: Colors.white70,
+          dayColor: Colors.teal[200],
+          dayNameColor: Color(0xFF333A47),
+          activeDayColor: Colors.white,
+          activeBackgroundDayColor: Colors.redAccent[100],
+          dotsColor: Color(0xFF333A47),
+          selectableDayPredicate: (date) => date.day != 0,
+          locale: 'en',
         ),
+        // Container(
+        //   color: Colors.blue,
+        //   child: DatePicker(
+        //     DateTime.now(),
+        //     initialSelectedDate: DateTime.now(),
+        //     selectionColor: Colors.black,
+        //     selectedTextColor: Colors.white,
+        //     daysCount:  15,
+        //     onDateChange: (date) {
+        //       // New date selected
+        //       setState(() {
+        //         selectedDay = date;
+        //       });
+        //     },
+        //   ),
+        // ),
         Expanded(
-            child: Scrollbar(
-              child: StreamBuilder<QuerySnapshot<task>>(
-                  stream: getTasks()
-                  .where('dateTime',isEqualTo: selectedDay.getDateOnly().millisecondsSinceEpoch)
-                      .snapshots(),
-                  builder: (BuildContext buildContext,
-                      AsyncSnapshot<QuerySnapshot<task>> snapshot) {
-                    if (snapshot.hasError) {
-                      return Text(snapshot.error.toString());
-                    } else if (snapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return Center(child: CircularProgressIndicator());
-                    }
-                    List<task> item =
-                    snapshot.data!.docs.map((e) => e.data()).toList();
-                    return ListView.builder(
-                      itemBuilder: (buildContext, index) {
-                        return TodoWidget(item[index]);
-                      },
-                      itemCount: item.length,
-                    );
-                  }),
-            )),
+          child: Scrollbar(
+            child: StreamBuilder<QuerySnapshot<task>>(
+                stream: getTasks()
+                .where('dateTime',isEqualTo: selectedDay.getDateOnly().millisecondsSinceEpoch)
+                    .snapshots(),
+                builder: (BuildContext buildContext,
+                    AsyncSnapshot<QuerySnapshot<task>> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text(snapshot.error.toString());
+                  } else if (snapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  List<task> item =
+                  snapshot.data!.docs.map((e) => e.data()).toList();
+                  return ListView.builder(
+                    itemBuilder: (buildContext, index) {
+                      return TodoWidget(item[index]);
+                    },
+                    itemCount: item.length,
+                  );
+                }),
+          ),
+        ),
       ],
     );
   }
